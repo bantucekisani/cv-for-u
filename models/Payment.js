bcbb1f3ff6b5db37c06e5b4c2a384ee3ff4b4b0b
Bantu Cekisani <bantucekisani@gmail.com>
2026-02-06 01:36:00 +0200

    Fix payment type enum for cover letter

--- models/Payment.js
+++ models/Payment.js
@@ -4,7 +4,7 @@ const PaymentSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     cvId: { type: mongoose.Schema.Types.ObjectId, ref: "CV" },
-    type: { type: String, enum: ["cv", "cover", "cover-letter"]
+    type: { type: String,  enum: ["cv", "cover-letter"]
 , required: true },
     amount: { type: Number, required: true },
     status: { type: String, default: "COMPLETE" },

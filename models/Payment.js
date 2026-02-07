5e742b35448aaefe83a885a83accddda3d61a402
Bantu Cekisani <bantucekisani@gmail.com>
2026-02-07 03:14:28 +0200

    fix: allow legacy cover payment enum to prevent IPN failure

--- models/Payment.js
+++ models/Payment.js
@@ -3,7 +3,12 @@ const PaymentSchema = new mongoose.Schema(
     paymentId: { type: String, unique: true }, // 🔥 ADD THIS
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     cvId: { type: mongoose.Schema.Types.ObjectId, ref: "CV" },
-    type: { type: String, enum: ["cv", "cover-letter"], required: true },
+   type: {
+  type: String,
+  enum: ["cv", "cover-letter", "cover"], // 👈 ADD THIS
+  required: true
+}
+,
     amount: { type: Number, required: true },
     status: { type: String, default: "COMPLETE" },
     provider: { type: String, default: "payfast" }
